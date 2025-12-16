7860f0acb07c84cac87f08d33cb09b099ff2e8e8
ESADavid <bizleeper@gmail.com>
2025-12-16 18:14:13 -0500

    UPDATE

--- src/database_fixed.py
+++ src/database_fixed.py
@@ -88,6 +88,7 @@ class TelemetryMetricsModel(Base):
 class BusinessModel(Base):
     """SQLAlchemy model for businesses"""
     __tablename__ = 'businesses'
+    __table_args__ = {'extend_existing': True}
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
